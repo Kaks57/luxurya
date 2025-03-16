c1e9884b9632949601389642a3174b9f42608277
Kaks57 <kmahtaj57@gmail.com>
2025-03-16 23:24:28 +0100

    Update Contact.tsx

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -116,8 +116,8 @@ const ContactInfo = () => (
   <div className="lg:pl-10 animate-slide-up" style={{ animationDelay: "0.2s" }}>
     <h2 className="text-2xl font-semibold mb-6">Nos coordonnées</h2>
     <CardInfo icon={<MapPin />} title="Adresse" details={["71 rue desnouettes", "75015, Paris, France"]} />
-    <CardInfo icon={<Phone />} title="Téléphone" details={["+33 6 72 74 06 85". "+33 7 53 91 07 12".    ]} />
-  </div>
+   <CardInfo icon={<Phone />} title="Téléphone" details={["+33 6 72 74 06 85", "+33 7 53 91 07 12"]} />
+</div>
 );
 
 const CardInfo = ({ icon, title, details }) => (
